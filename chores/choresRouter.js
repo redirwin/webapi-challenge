c2f74cbaaf6814e8bfe133d9255fc5c0f86eba58
Dave Irwin <david@redirwin.com>
2019-08-09 08:47:14 -0700

    Created empty endpoints for chores CRUD operations.

--- chores/choresRouter.js
+++ chores/choresRouter.js
@@ -1,7 +1,15 @@
-const express = require("expres");
+const express = require("express");
 
 const router = express.Router();
 
 const Chores = require("./choresDb");
+
+router.post("/", (req, res) => {});
+
+router.get("/", (req, res) => {});
+
+router.put("/", (req, res) => {});
+
+router.delete("/", (req, res) => {});
 
 module.exports = router;

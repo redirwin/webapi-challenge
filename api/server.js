24410441df7a43e201dded738cd0d7bdb08b9fa4
Dave Irwin <david@redirwin.com>
2019-08-09 08:35:42 -0700

    Created empty databases and chores router file.

--- api/server.js
+++ api/server.js
@@ -1,9 +1,10 @@
 const express = require("express");
+const choresRouter = require("../chores/choresDb");
 
 const server = express();
 
 server.get("/", (req, res) => {
-    res.status(200).json({ message: "local server" });
-  });
-  
-  module.exports = server;
+  res.status(200).json({ message: "local server" });
+});
+
+module.exports = server;

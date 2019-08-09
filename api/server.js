95bf820dcb8c1bdabc1723a420527eed78cffd8e
Dave Irwin <david@redirwin.com>
2019-08-09 09:14:43 -0700

    Created people router and people seed arrray with data.

--- api/server.js
+++ api/server.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const choresRouter = require("../routers/choresRouter");
+const peopleRouter = require("../routers/peopleRouter");
 
 const server = express();
 
 server.use(express.json());
 server.use("/:personId/chores", choresRouter);
+server.use("/", peopleRouter);
 
 server.get("/", (req, res) => {
   res.status(200).json({ message: "local server" });
--- routers/peopleRouter.js
+++ routers/peopleRouter.js
@@ -2,12 +2,15 @@ const express = require("express");
 
 const router = express.Router();
 
-let chores = [];
+let people = [
+  { id: 1, name: "Robert Jordan" },
+  { id: 2, name: "Rand al'Thor" }
+];
 
-let choresId = 0;
+let peopleId = 2;
 
 router.post("/", (req, res) => {
-  chores.push(req.body);
+  people.push(req.body);
 });
 
 router.get("/", (req, res) => {});

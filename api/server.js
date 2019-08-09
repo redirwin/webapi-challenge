ed412de50bde67162d2bf40bfd15b6313425f077
Dave Irwin <david@redirwin.com>
2019-08-09 08:38:49 -0700

    Added chores router to index at for use at /chores.

--- api/server.js
+++ api/server.js
@@ -1,7 +1,10 @@
 const express = require("express");
-const choresRouter = require("../chores/choresDb");
+const choresRouter = require("../chores/choresRouter");
 
 const server = express();
+
+server.use(express.json());
+server.use("/chores", choresRouter);
 
 server.get("/", (req, res) => {
   res.status(200).json({ message: "local server" });
---
+++ chores/choresRouter.js
@@ -0,0 +1,7 @@
+const express = require("expres");
+
+const router = express.Router();
+
+const Chores = require("./choresDb");
+
+module.exports = router;
